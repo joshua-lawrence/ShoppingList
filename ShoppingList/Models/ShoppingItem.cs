367cae35f70b371fa976edb00f18dcc93239c55f
Joshua Lawrence <joshua98lawrence@gmail.com>
2020-09-23 04:29:56 -0600

    Configuring model

---
+++ ShoppingList/Models/ShoppingItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShoppingList.Models
+{
+    public class ShoppingItem
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Quantity { get; set; }
+        public bool Purchased { get; set; }
+    }
+}
